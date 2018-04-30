7700289635d706bec5497c7755244bca6614b098
Babuta Dragos <bdie1857@scs.ubbcluj.ro>
2018-04-30 20:18:32 +0300

    l4-2

---
+++ src/test/java/IncrementalIntegretionTest.java
@@ -0,0 +1,56 @@
+import controller.EntryController;
+import model.Entry;
+import org.junit.Before;
+import org.junit.Test;
+import repository.EntryRepository;
+import repository.MemberRepository;
+
+import java.util.List;
+
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+
+public class IncrementalIntegretionTest {
+    EntryRepository re;
+    MemberRepository rm;
+    EntryController c;
+    @Before
+    public void setUp() throws Exception{
+        re=new EntryRepository();
+        rm=new MemberRepository();
+        c=new EntryController(re, rm);
+        rm.addMember("Jon",1);
+    }
+
+    @Test
+    public void testCaseA(){
+        try {
+            rm.addMember("Max",2);
+        } catch (Exception e) {
+            fail(e.getMessage());
+        }
+    }
+
+    @Test
+    public void testCaseAB(){
+        try {
+            rm.addMember("Max",4);
+            c.addEntry("cost",4,4);
+        } catch (Exception e) {
+            fail(e.getMessage());
+        }
+    }
+
+
+    @Test
+    public void testCaseALL(){
+        try {
+            rm.addMember("Tom",4);
+            c.addEntry("cost",4,4);
+            List<Entry> list=c.allEntriesForMember(4);
+            assertTrue(1==list.size());
+        } catch (Exception e) {
+            fail(e.getMessage());
+        }
+    }
+}
